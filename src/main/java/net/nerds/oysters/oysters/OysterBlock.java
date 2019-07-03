858d849d96747c0fdfe78b45de804cdfd1342342
q1422955 <kyler.love@qvc.com>
2019-07-03 13:12:24 -0400

    block facing changes

--- src/main/java/net/nerds/oysters/oysters/OysterBlock.java
+++ src/main/java/net/nerds/oysters/oysters/OysterBlock.java
@@ -1,18 +1,18 @@
 package net.nerds.oysters.oysters;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.Waterloggable;
+import net.minecraft.block.*;
 import net.minecraft.entity.EntityContext;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateFactory;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.state.property.Property;
 import net.minecraft.tag.FluidTags;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
@@ -22,11 +22,14 @@ import net.minecraft.world.IWorld;
 public class OysterBlock extends Block implements Waterloggable {
 
     private VoxelShape voxelShape = Block.createCuboidShape(5.0D, 0.0D, 5.0D, 11.0D, 4.0D, 13.0D);
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     public OysterBlock(Settings blockSettings) {
         super(blockSettings);
-        this.setDefaultState(getStateFactory().getDefaultState().with(WATERLOGGED, true));
+        this.setDefaultState(getStateFactory().getDefaultState()
+                .with(WATERLOGGED, true)
+                .with(FACING, Direction.NORTH));
     }
 
     @Override
@@ -38,7 +41,19 @@ public class OysterBlock extends Block implements Waterloggable {
     public BlockState getPlacementState(ItemPlacementContext itemPlacementContext) {
             FluidState fluidState = itemPlacementContext.getWorld().getFluidState(itemPlacementContext.getBlockPos());
             boolean waterLog = fluidState.matches(FluidTags.WATER) && fluidState.getLevel() == 8;
-            return super.getPlacementState(itemPlacementContext).with(WATERLOGGED, waterLog);
+            return super.getPlacementState(itemPlacementContext)
+                    .with(WATERLOGGED, waterLog)
+                    .with(FACING, itemPlacementContext.getPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState blockState, BlockRotation rotation) {
+        return blockState.with(FACING, rotation.rotate(blockState.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState blockState, BlockMirror mirror) {
+        return blockState.rotate(mirror.getRotation(blockState.get(FACING)));
     }
 
     @Override
@@ -48,6 +63,6 @@ public class OysterBlock extends Block implements Waterloggable {
 
     @Override
     protected void appendProperties(StateFactory.Builder<Block, BlockState> stateBuilder) {
-        stateBuilder.add(WATERLOGGED);
+        stateBuilder.add(WATERLOGGED, FACING);
     }
 }
